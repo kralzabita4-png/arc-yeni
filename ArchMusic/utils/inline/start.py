6180dd0dadf53113e80491d334cdc9456093e9b0
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-10-20 00:02:18 +0300

    Update start.py

--- ArchMusic/utils/inline/start.py
+++ ArchMusic/utils/inline/start.py
@@ -1,127 +1,48 @@
-# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
-#
-# This file is part of < https://github.com/ArchBots/ArchMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from config import SUPPORT_CHANNEL, SUPPORT_GROUP
 from ArchMusic import app
 
 
 def start_pannel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_2"], callback_data="settings_helper"
-            ),
-        ],
+            InlineKeyboardButton(text=_["S_B_1"], url=f"https://t.me/{app.username}?start=help"),
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+        ]
     ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                ),
-                InlineKeyboardButton(
-                    text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                ),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL and not SUPPORT_GROUP:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                    )
-                ]
-            )
-        elif SUPPORT_GROUP and not SUPPORT_CHANNEL:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    )
-                ]
-            )
+
+    # Destek butonları
+    support_buttons = []
+    if SUPPORT_CHANNEL:
+        support_buttons.append(InlineKeyboardButton(text=_["S_B_4"], url=SUPPORT_CHANNEL))
+    if SUPPORT_GROUP:
+        support_buttons.append(InlineKeyboardButton(text=_["S_B_3"], url=SUPPORT_GROUP))
+
+    if support_buttons:
+        buttons.append(support_buttons)
+
     return buttons
 
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_8"], callback_data="settings_back_helper"
-            )
-        ],
-    ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                ),
-                InlineKeyboardButton(
-                    text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                ),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL and not SUPPORT_GROUP:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                    )
-                ]
-            )
-        elif SUPPORT_GROUP and not SUPPORT_CHANNEL:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    )
-                ]
-            )
+    buttons = [[InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]]
 
-    buttons.append(
-        [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ]
-    )
-    if GITHUB_REPO and OWNER:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER)
-            ]
-        )
-    else:
-        if GITHUB_REPO and not OWNER:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"https://github.com/ArchBots/ArchMusic"
-                    )
-                ]
-            )
-        if OWNER and not GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_7"], user_id=OWNER
-                    )
-                ]
-            )
+    # Destek ve Owner butonları aynı satırda
+    support_buttons = []
+    if SUPPORT_CHANNEL:
+        support_buttons.append(InlineKeyboardButton(text=_["S_B_4"], url=SUPPORT_CHANNEL))
+    if SUPPORT_GROUP:
+        support_buttons.append(InlineKeyboardButton(text=_["S_B_3"], url=SUPPORT_GROUP))
+    if OWNER:
+        support_buttons.append(InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER))
+
+    if support_buttons:
+        buttons.append(support_buttons)
+
+    # Botu gruba ekleme butonu
+    buttons.append([InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/{BOT_USERNAME}?startgroup=true")])
+
     return buttons
+                                                    
