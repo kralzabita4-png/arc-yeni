f237c829658e0d41a170901946002b93882a50d4
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-10-19 20:57:00 +0300

    Update skip.py

--- ArchMusic/plugins/admins/skip.py
+++ ArchMusic/plugins/admins/skip.py
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
 #
 # This file is part of < https://github.com/ArchBots/ArchMusic > project,
@@ -8,7 +7,7 @@
 # All rights reserved.
 
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, Message
+from pyrogram.types import Message
 
 import config
 from config import BANNED_USERS
@@ -18,8 +17,7 @@ from ArchMusic.core.call import ArchMusic
 from ArchMusic.misc import db
 from ArchMusic.utils.database import get_loop
 from ArchMusic.utils.decorators import AdminRightsCheck
-from ArchMusic.utils.inline.play import (stream_markup,
-                                          telegram_markup)
+from ArchMusic.utils.inline.play import stream_markup
 from ArchMusic.utils.stream.autoclear import auto_clean
 
 
@@ -52,14 +50,9 @@ async def skip(cli, message: Message, _, chat_id):
                             try:
                                 popped = check.pop(0)
                             except:
-                                return await message.reply_text(
-                                    _["admin_16"]
-                                )
+                                return await message.reply_text(_["admin_16"])
                             if popped:
-                                if (
-                                    config.AUTO_DOWNLOADS_CLEAR
-                                    == str(True)
-                                ):
+                                if config.AUTO_DOWNLOADS_CLEAR == str(True):
                                     await auto_clean(popped)
                             if not check:
                                 try:
@@ -73,9 +66,7 @@ async def skip(cli, message: Message, _, chat_id):
                                     return
                                 break
                     else:
-                        return await message.reply_text(
-                            _["admin_15"].format(count)
-                        )
+                        return await message.reply_text(_["admin_15"].format(count))
                 else:
                     return await message.reply_text(_["admin_14"])
             else:
@@ -106,39 +97,35 @@ async def skip(cli, message: Message, _, chat_id):
                 return await ArchMusic.stop_stream(chat_id)
             except:
                 return
+
     queued = check[0]["file"]
     title = (check[0]["title"]).title()
     user = check[0]["by"]
     streamtype = check[0]["streamtype"]
     videoid = check[0]["vidid"]
     status = True if str(streamtype) == "video" else None
+
     if "live_" in queued:
         n, link = await YouTube.video(videoid, True)
         if n == 0:
-            return await message.reply_text(
-                _["admin_11"].format(title)
-            )
+            return await message.reply_text(_["admin_11"].format(title))
         try:
             await ArchMusic.skip_stream(chat_id, link, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
-        button = telegram_markup(_, chat_id)
-        img = None
         run = await message.reply_text(
-                text=_["stream_1"].format(
-                  title,
-                    f"https://t.me/{app.username}?start=info_{videoid}",
-                    check[0]["dur"],
-                    user,
-                ),
-                
-            )
-        db[chat_id][0]["mystic"] = run
-        db[chat_id][0]["markup"] = "tg"
-    elif "vid_" in queued:
-        mystic = await message.reply_text(
-            _["call_10"], disable_web_page_preview=True
+            text=_["stream_1"].format(
+                title,
+                f"https://t.me/{app.username}?start=info_{videoid}",
+                check[0]["dur"],
+                user,
+            ),
         )
+        db[chat_id][0]["mystic"] = run
+        db[chat_id][0]["markup"] = None
+
+    elif "vid_" in queued:
+        mystic = await message.reply_text(_["call_10"], disable_web_page_preview=True)
         try:
             file_path, direct = await YouTube.download(
                 videoid,
@@ -152,73 +139,46 @@ async def skip(cli, message: Message, _, chat_id):
             await ArchMusic.skip_stream(chat_id, file_path, video=status)
         except Exception:
             return await mystic.edit_text(_["call_9"])
-        button = stream_markup(_, videoid, chat_id)
-        img = None
         run = await message.reply_text(
-                text=_["stream_1"].format(
-                  title,
-                    f"https://t.me/{app.username}?start=info_{videoid}",
-                    check[0]["dur"],
-                    user,
-                ),
-                
-            )
+            text=_["stream_1"].format(
+                title,
+                f"https://t.me/{app.username}?start=info_{videoid}",
+                check[0]["dur"],
+                user,
+            ),
+        )
         db[chat_id][0]["mystic"] = run
-        db[chat_id][0]["markup"] = "stream"
+        db[chat_id][0]["markup"] = None
         await mystic.delete()
+
     elif "index_" in queued:
         try:
             await ArchMusic.skip_stream(chat_id, videoid, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
-        button = telegram_markup(_, chat_id)
         run = await message.reply_text(
-                text=_["stream_2"].format(
-                  title,
-                    f"https://t.me/{app.username}?start=info_{videoid}",
-                    check[0]["dur"],
-                    user,
-                ),
-                
-            )
+            text=_["stream_2"].format(
+                title,
+                f"https://t.me/{app.username}?start=info_{videoid}",
+                check[0]["dur"],
+                user,
+            ),
+        )
         db[chat_id][0]["mystic"] = run
-        db[chat_id][0]["markup"] = "tg"
+        db[chat_id][0]["markup"] = None
+
     else:
         try:
             await ArchMusic.skip_stream(chat_id, queued, video=status)
         except Exception:
             return await message.reply_text(_["call_9"])
-        if videoid == "telegram":
-            button = telegram_markup(_, chat_id)
-            run = await message.reply_text(
-                text=_["stream_3"].format(
-                    title, check[0]["dur"], user
-                ),
-                
-            )
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "tg"
-        elif videoid == "soundcloud":
-            button = telegram_markup(_, chat_id)
-            run = await message.reply_text(
-                text=_["stream_3"].format(
-                    title, check[0]["dur"], user
-                ),
-                
-            )
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "tg"
-        else:
-            button = stream_markup(_, videoid, chat_id)
-            img = None
-            run = await message.reply_text(
-                text=_["stream_1"].format(
-                  title,
-                    f"https://t.me/{app.username}?start=info_{videoid}",
-                    check[0]["dur"],
-                    user,
-                ),
-                
-            )
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "stream"
+        run = await message.reply_text(
+            text=_["stream_1"].format(
+                title,
+                f"https://t.me/{app.username}?start=info_{videoid}",
+                check[0]["dur"],
+                user,
+            ),
+        )
+        db[chat_id][0]["mystic"] = run
+        db[chat_id][0]["markup"] = None
